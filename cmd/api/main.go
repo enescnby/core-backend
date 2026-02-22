3766797fcacd6b178443c9132b2b33efeeb9fa78
enescnby <enes1canbay@gmail.com>
2026-02-23 00:31:05 +0300

    feat(auth): implement zero-knowledge challenge-response login flow with DTOs, handlers, Ed25519 verification and JWT middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"core-backend/internal/config"
 	"core-backend/internal/database"
+	"core-backend/internal/handlers"
+	"core-backend/internal/repositories"
+	"core-backend/internal/services"
 	"core-backend/pkg/logger"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func main() {
+	//Logger
 	logger.InitLogger()
 	defer logger.Log.Sync()
 
@@ -16,4 +24,26 @@ func main() {
 	defer database.Close()
 
 	database.Migrate()
+
+	userRepo := repositories.NewUserRepository()
+	auditRepo := repositories.NewAuditRepository()
+
+	authService := services.NewAuthService(userRepo, auditRepo)
+
+	authHandler := handlers.NewAuthHandler(authService)
+
+	app := fiber.New(fiber.Config{
+		AppName: "CoreGuard E2EE Backend v0.1.0",
+	})
+
+	api := app.Group("/api/v1")
+	api.Post("/register", authHandler.Register)
+	api.Post("/login/init", authHandler.LoginInit)
+	api.Post("/login/verify", authHandler.LoginVerify)
+	
+	logger.Log.Info(fmt.Sprintf("starting CoreGuard server on port %s", config.AppConfig.AppPort))
+
+	if err := app.Listen(config.AppConfig.AppPort); err != nil {
+		logger.Log.Fatal("server failed to start", zap.Error(err))
+	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fasthttp/websocket v1.5.8 // indirect
 	github.com/gofiber/contrib/websocket v1.3.4 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
